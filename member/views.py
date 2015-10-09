edff4effea0fed0b9a48d6cd104cf856e8163a47
Sam Liu <genxstylez@gmail.com>
2015-10-10 04:20:18 +0800

    signup page done

--- member/views.py
+++ member/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from member.forms import SignupForm
+from django.contrib.auth import authenticate, login
 
 
 def new_social(request):
@@ -9,10 +10,17 @@ def new_social(request):
         request.session['username'] = form.cleaned_data['username']
         request.session['password'] = form.cleaned_data['password']
         return redirect('social:complete', backend=backend)
-    return render(request, 'member/new_user.html', {'form': form})
+    return render(request, 'member/signup.html', {'form': form})
 
 
 def signup(request):
-    return render(request, 'member/new_user.html')
+    form = SignupForm(request.POST or None)
+    if form.is_valid():
+        user = form.save()
+        user = authenticate(user.username, user.password)
+        if user is not None and user.is_active:
+            login(request, user)
+            redirect('index')
+    return render(request, 'member/signup.html')
 
 
