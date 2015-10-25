8c4497c441e64332f1ccb75864d7e673b9f60768
Sam Liu <genxstylez@gmail.com>
2015-10-26 06:07:30 +0800

    add gunicorn and gevent

--- mooq/wsgi.py
+++ mooq/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mooq.settings")
 
+def get_application():
+    return reduce(lambda app, wrapper: wrapper(app), [], get_wsgi_application())
+
 application = get_wsgi_application()
