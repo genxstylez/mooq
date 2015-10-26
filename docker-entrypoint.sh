c739109341b4455a0a087534ba44bd8f3aa279c1
Sam Liu <genxstylez@gmail.com>
2015-10-26 17:55:33 +0800

    trying AWSElasticbeanstalk

---
+++ docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$1" = 'gunicorn' ]; then
+    echo Starting Gunicorn.
+    python manage.py migrate --noinput        # Apply database migrations
+    python manage.py collectstatic --noinput  # Collect static files
+    exit $?
+fi
+
+exec "$@"
